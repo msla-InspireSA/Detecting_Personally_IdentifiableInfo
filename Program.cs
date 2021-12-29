e49d6cb2465bbdde6cf4d070bc18cdcfff9863c5
msla-InspireSA <80754340+msla-InspireSA@users.noreply.github.com>
2021-12-29 23:36:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Azure;
+using System;
+using Azure.AI.TextAnalytics;
+
+namespace Detecting_Personally_IdentifiableInfo
+{
+
+    internal class Program
+    {
+        //connection to azure
+        private static readonly AzureKeyCredential credentials = new AzureKeyCredential("add your own key");
+        private static readonly Uri endpoint = new Uri("add your own endpoint");
+
+        // Example method for detecting sensitive information (PII) from text 
+        static void RecognizePIIExample(TextAnalyticsClient client)
+        {
+            string document = "A developer with SSN 859-98-0987 whose phone number is 800-102-1100 is building tools with our APIs.";
+
+            PiiEntityCollection entities = client.RecognizePiiEntities(document).Value;
+
+            Console.WriteLine($"Redacted Text: {entities.RedactedText}");
+            if (entities.Count > 0)
+            {
+                Console.WriteLine($"Recognized {entities.Count} PII entit{(entities.Count > 1 ? "ies" : "y")}:");
+                foreach (PiiEntity entity in entities)
+                {
+                    Console.WriteLine($"Text: {entity.Text}, Category: {entity.Category}, SubCategory: {entity.SubCategory}, Confidence score: {entity.ConfidenceScore}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No entities were found.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var client = new TextAnalyticsClient(endpoint, credentials);
+            RecognizePIIExample(client);
+
+            Console.Write("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
